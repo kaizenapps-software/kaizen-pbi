a8fbf29809635fbf75749a4497b808b564839585
EngelAlexey <desarrollo@kaizenapps.net>
2025-09-23 12:28:04 -0600

    refactor: format vite.config.js for better readability

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { "/auth": { target: "http://localhost:3001", changeOrigin: true, secure: false } } }
+  server: {
+    port: 5173,
+    proxy: {
+      '/auth': {
+        target: 'http://localhost:3001', 
+        changeOrigin: true
+      }
+    }
+  }
 })
